2272accf50c7c45eb102740d0f4552fcdac13868
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-04-25 22:12:22 +0100

    Modify TSL-config.sh for SSL certificate generation
    Updated script to use sh instead of bash and added SSL certificate generation commands with explanations.

--- INCIPTION/src/nginx/TSL-config.sh
+++ INCIPTION/src/nginx/TSL-config.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 #use this script to run ther files and use the code inside of the configue and the dockerfile use to run it automatic li 
+#install the ssl certificate script
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout myndfun.key -out myndfin.crt
+#openssl : use to  encryption , certifacates keys
+#-req :certificat requarment (use to creat the csr or directly creat the certificat like what i do )("-x509"=>gnerat the self-segned certificat directly)
+#-noods : No DES (no encryption) beacaus docker conainer must start automaticlly no interactive password
+#newkey : this to creat the new key and rsa -> encryption algorithm and the 2048-> this is the key size
+#keyout myndfun.key this is the output file for the privetkey "secretfile
+#out mynfin.crt: this is the output file certificat "public file"
